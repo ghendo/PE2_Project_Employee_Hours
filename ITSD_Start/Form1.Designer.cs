cbcc235c3168a5c6457afaac8528f599bd00a3fa
Graeme Henderson <ghendo@me.com>
2018-01-06 17:18:30 +1100

    6/1/18T5:18

--- ITSD_Start/Form1.Designer.cs
+++ ITSD_Start/Form1.Designer.cs
@@ -610,7 +610,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1152, 810);
+            this.tabPage2.Size = new System.Drawing.Size(1149, 810);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Customers";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -725,7 +725,7 @@
             this.tabPage3.Controls.Add(this.lblRecyclerTabRecyclerID);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1152, 810);
+            this.tabPage3.Size = new System.Drawing.Size(1149, 810);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Recyclers";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -840,7 +840,7 @@
             this.AutoScroll = true;
             this.AutoScrollMargin = new System.Drawing.Size(5, 5);
             this.AutoScrollMinSize = new System.Drawing.Size(5, 5);
-            this.ClientSize = new System.Drawing.Size(1178, 813);
+            this.ClientSize = new System.Drawing.Size(1195, 758);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             // 
